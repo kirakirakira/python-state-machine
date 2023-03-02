2212d68ac88d5eba778521b0f73a5cb3e7c3d33a
Kira Hartlage <kira.hartlage@gmail.com>
2023-03-02 13:55:15 -0500

    mocks not working

---
+++ example/statey.py
@@ -0,0 +1,5 @@
+from state_machine import StateMachine
+
+state_machine = StateMachine("running")
+
+state_machine.transition("stopped")
--- src/state_machine.py
+++ src/state_machine.py
@@ -1,9 +1,13 @@
 class StateMachine:
     def __init__(self, initialState) -> None:
         self.currentState = initialState
+        self.sendSignal("ENTRY", None)
 
     def transition(self, targetState):
-        pass
+        self.sendSignal("EXIT", None)
+        self.currentState = targetState
+        self.sendSignal("ENTRY", None)
 
     def sendSignal(self, signal, data):
+        # self.currentState(self, signal, data)
         pass
