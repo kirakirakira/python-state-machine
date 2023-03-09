bf2308d551c297d40d8c808fe3b8e09d6f17877c
Kira Hartlage <kira.hartlage@gmail.com>
2023-03-09 13:50:39 -0500

    created an example with timer and state machine from chatgpt

--- src/state_machine.py
+++ src/state_machine.py
@@ -1,13 +1,36 @@
 class StateMachine:
     def __init__(self, initialState) -> None:
         self.currentState = initialState
-        self.sendSignal("ENTRY", None)
+        self.signals = {}
+        # self.sendSignal("ENTRY", None)
 
     def transition(self, targetState):
-        self.sendSignal("EXIT", None)
+        # self.sendSignal("EXIT", None)
         self.currentState = targetState
-        self.sendSignal("ENTRY", None)
+        # self.sendSignal("ENTRY", None)
 
-    def sendSignal(self, signal, data):
+    def addSignal(self, targetState, signal):
+        if targetState not in self.signals:
+            self.signals[targetState] = signal
+        else:
+            self.signals[targetState].append(signal)
         # self.currentState(self, signal, data)
-        pass
+
+    def getState(self):
+        return self.currentState
+
+    def sendSignal(self, signal):
+        for state, signals in self.signals.items():
+            if signal in signals:
+                print("you got it")
+
+
+sm = StateMachine("off")
+print(sm.getState())
+
+sm.transition("on")
+print(sm.getState())
+
+sm.addSignal("off", "go to on")
+
+sm.sendSignal("go to on")
