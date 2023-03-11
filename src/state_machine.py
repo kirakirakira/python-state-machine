7edd569b0500830b6daff840613065f4df42362f
Kira Hartlage <kira.hartlage@gmail.com>
2023-03-11 13:12:12 -0500

    j/k it works

--- src/state_machine.py
+++ src/state_machine.py
@@ -4,14 +4,13 @@ SIGNAL_EXIT = 1
 SIGNAL_USER_START = 2
 
 class fsm:
-    def __init__(self, current = None):
-        self.current = current
+    def __init__(self, initial):
+        self.current = initial(self, SIGNAL_ENTRY, None)
 
-    # Initializes an FSM with the specified initial state. Sends the entry signal to the
-    # initial state.
-    def init(self, initial):
-        self.current = initial
-        self.current(self, SIGNAL_ENTRY, None)
+    # # Initializes an FSM with the specified initial state. Sends the entry signal to the
+    # # initial state.
+    # def init(self, initial):
+    #     self.current = initial
 
     # Sends a signal and optional signal data to the current state.
     def send_signal(self, signal, data):
@@ -21,5 +20,4 @@ class fsm:
     # state, then sends entry to the target state.
     def transition(self, target):
         self.current(self, SIGNAL_EXIT, None)
-        self.current = target
-        self.current(self, SIGNAL_ENTRY, None)
+        self.current = target(self, SIGNAL_ENTRY, None)
--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -1,26 +1,17 @@
 import unittest
 from src.state_machine import fsm, SIGNAL_ENTRY, SIGNAL_EXIT, SIGNAL_USER_START
 from unittest import mock
-from unittest.mock import patch, call
-
-# def initial_state(fsm, signal, data):
-#     pass
 
 class testfsm(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_init(self):
-        initial_state = mock.Mock()
-        self.fsm = fsm(current = initial_state)
-        self.fsm.init(initial_state)
-        initial_state.assert_called_once_with(self.fsm, SIGNAL_ENTRY, None)
+        initial_state = mock.Mock(return_value=None)
+        fsm_instance = fsm(initial_state)
 
-    def test_transition(self):
-        new_state_machine = fsm("running")
-
-    def test_send_signal(self):
-        new_state_machine = fsm("running")
+        self.assertEqual(initial_state.call_count, 1)
+        initial_state.assert_called_once_with(fsm_instance, SIGNAL_ENTRY, None)
 
 if __name__ == '__main__':
     unittest.main()
