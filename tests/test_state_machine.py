d9d71bd2b02c10b7edbb9e6f95fb0593ca8b6ded
Kira Hartlage <kira.hartlage@gmail.com>
2023-03-11 13:27:24 -0500

    test entry and exit

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -6,12 +6,32 @@ class testfsm(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_init(self):
+    def test_init_sends_entry_signal(self):
         initial_state = mock.Mock(return_value=None)
         fsm_instance = fsm(initial_state)
 
         self.assertEqual(initial_state.call_count, 1)
         initial_state.assert_called_once_with(fsm_instance, SIGNAL_ENTRY, None)
+
+    def test_transition_sends_exit_and_entry_signals(self):
+        current_state_mock = mock.Mock()
+        target_state_mock = mock.Mock(return_value=None)
+
+        fsm_instance = fsm(current_state_mock)
+        fsm_instance.transition(target_state_mock)
+
+        current_state_mock.assert_has_calls(
+            [
+                mock.call(fsm_instance, SIGNAL_ENTRY, None),
+                mock.call(fsm_instance, SIGNAL_EXIT, None)
+            ]
+        )
+
+        target_state_mock.assert_has_calls(
+            [
+                mock.call(fsm_instance, SIGNAL_ENTRY, None)
+            ]
+        )
 
 if __name__ == '__main__':
     unittest.main()
